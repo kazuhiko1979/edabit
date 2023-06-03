8f729d57ff55c46ea7ed494779ae85d1b51a9093
kazuhiko1979 <challengevolley1979@gmail.com>
2023-06-03 09:54:21 +0900

    create 092_very_hard

---
+++ 092_very_hard_Pascal's Triangle.py
@@ -0,0 +1,24 @@
+"""
+Pascal's Triangle
+The goal of this challenge is to return Pascal's triangle up to number 29. Pascal's triangle is the sum of the two upper corners.
+
+   1 1
+  1 2 1
+ 1 3 3 1
+
+# There will always be the 1 in the first
+# place and the row in the second.
+Pascal's Triangle
+
+Create a function that returns a row from Pascal's triangle. To find the row and column you can use n!/(k!*(n-k)!) where n is the row down and k is the column.
+
+Examples
+pascals_triangle(1) ➞ "1 1"
+
+pascals_triangle(4) ➞ "1 4 6 4 1"
+
+pascals_triangle(6) ➞ "1 6 15 20 15 6 1"
+
+pascals_triangle(8) ➞ "1 8 28 56 70 56 28 8 1"
+"""
+

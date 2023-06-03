0415220207988699d1f587c86637f0d5d9679c22
kazuhiko1979 <challengevolley1979@gmail.com>
2023-06-03 11:37:03 +0900

    update 092

--- 092_very_hard_Pascal's Triangle.py
+++ 092_very_hard_Pascal's Triangle.py
@@ -21,36 +21,55 @@ pascals_triangle(6) ➞ "1 6 15 20 15 6 1"
 
 pascals_triangle(8) ➞ "1 8 28 56 70 56 28 8 1"
 """
-import copy
-
 def pascals_triangle(row):
 
-	count = 0
+	if row == 1:
+		return ' '.join(map(str, [1, 1]))
 
-	def set_list(top_cur_list, bottom_cur_list, count):
+	triangle = [[1]]
 
-		cur_list = []
-		top_cur_list.insert(0, 0)
-		bottom_cur_list.append(0)
+	for i in range(1, row):
+		prev_row = triangle[-1]
+		cur_row = [1]
 
-		for i, k in zip(top_cur_list, bottom_cur_list):
-			cur_list.append(i + k)
+		for j in range(1, i):
+			cur_row.append(prev_row[j-1] + prev_row[j])
 
-		count += 1
+		cur_row.append(1)
+		triangle.append(cur_row)
 
-		if count < row:
-			temp_cur_list = copy.deepcopy(cur_list)
-			return set_list(cur_list, temp_cur_list, count)
-		else:
-			return ' '.join(map(str, cur_list[1:-1]))
+	return ' '.join(map(str, triangle[-1]))
 
-	while row >= count:
-		# Initial settings
-		top_pre_list = [0, 1, 0]
-		bottom_pre_list = [0, 1, 0]
-
-		return set_list(top_pre_list, bottom_pre_list, count)
-
+# import copy
+#
+# def pascals_triangle(row):
+#
+# 	count = 0
+#
+# 	def set_list(top_cur_list, bottom_cur_list, count):
+#
+# 		cur_list = []
+# 		top_cur_list.insert(0, 0)
+# 		bottom_cur_list.append(0)
+#
+# 		for i, k in zip(top_cur_list, bottom_cur_list):
+# 			cur_list.append(i + k)
+#
+# 		count += 1
+#
+# 		if count < row:
+# 			temp_cur_list = copy.deepcopy(cur_list)
+# 			return set_list(cur_list, temp_cur_list, count)
+# 		else:
+# 			return ' '.join(map(str, cur_list[1:-1]))
+#
+# 	while row >= count:
+# 		# Initial settings
+# 		top_pre_list = [0, 1, 0]
+# 		bottom_pre_list = [0, 1, 0]
+#
+# 		return set_list(top_pre_list, bottom_pre_list, count)
+#
 print(pascals_triangle(1))
 print(pascals_triangle(2))
 print(pascals_triangle(3))

b65e347dad06b27aac0cae01bdac6838f19df47a
kazuhiko1979 <challengevolley1979@gmail.com>
2023-08-05 12:23:21 +0900

    update

--- 099_very_hard_Sudoku Validation.py
+++ 099_very_hard_Sudoku Validation.py
@@ -31,49 +31,75 @@ sudoku_validator([
 ]) ➞ False
 """
 
-import itertools
-import numpy as np
-
 def sudoku_validator(x):
 
-    one = []
-    two = []
-    three = []
+    # リスト内、数字のユニークチェック
+    def is_unique(nums):
+        seen = set()
+        for num in nums:
+            if num != 0 and num in seen:
+                return False
+            seen.add(num)
+        return True
 
-    sudoku_cross = list(itertools.chain.from_iterable(x))
-    count = 1
+    # 行列チェック
+    for i in range(9):
+        if not is_unique(x[i]) or not is_unique([x[j][i] for j in range(9)]):
+            return False
 
-    for i, n in enumerate(sudoku_cross, start=1):
-        if 1 <= count <= 3:
-            one.append(n)
-            count += 1
-            continue
-        if 4 <= count <= 6:
-            two.append(n)
-            count +=1
-            continue
-        if 7 <= count <= 9:
-            three.append(n)
-            count += 1
-            continue
-        if count == 10:
-            count = 2
-            one.append(n)
-            continue
+    # 3x3 boxes チェック
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            box = [x[z][y] for z in range(i, i + 3) for y in range(j, j + 3)]
+            if not is_unique(box):
+                return False
 
-    x_t = np.array(x).T.tolist()
+    return True
 
-    sudoku_crosses = [one, two, three]
 
-    for sudoku_cross in sudoku_crosses:
-        for i in range(0, len(sudoku_cross), 9):
-            x.append(sudoku_cross[i:i+9])
-
-    for t in x_t:
-        x.append(t)
-
-    result = [len(set(l)) == 9 for l in x]
-    return all(result)
+# import itertools
+# import numpy as np
+#
+# def sudoku_validator(x):
+#
+#     one = []
+#     two = []
+#     three = []
+#
+#     sudoku_cross = list(itertools.chain.from_iterable(x))
+#     count = 1
+#
+#     for i, n in enumerate(sudoku_cross, start=1):
+#         if 1 <= count <= 3:
+#             one.append(n)
+#             count += 1
+#             continue
+#         if 4 <= count <= 6:
+#             two.append(n)
+#             count +=1
+#             continue
+#         if 7 <= count <= 9:
+#             three.append(n)
+#             count += 1
+#             continue
+#         if count == 10:
+#             count = 2
+#             one.append(n)
+#             continue
+#
+#     x_t = np.array(x).T.tolist()
+#
+#     sudoku_crosses = [one, two, three]
+#
+#     for sudoku_cross in sudoku_crosses:
+#         for i in range(0, len(sudoku_cross), 9):
+#             x.append(sudoku_cross[i:i+9])
+#
+#     for t in x_t:
+#         x.append(t)
+#
+#     result = [len(set(l)) == 9 for l in x]
+#     return all(result)
 
 print(sudoku_validator([
   [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
@@ -108,7 +134,9 @@ print(sudoku_validator(
   [ 7, 1, 3, 9, 2, 4, 8, 5, 6 ],
   [ 9, 6, 1, 5, 3, 7, 2, 8, 4 ],
   [ 2, 8, 7, 4, 1, 9, 6, 3, 5 ],
-  [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ] ]))print(sudoku_validator([
+  [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ] ]))
+
+print(sudoku_validator([
   [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
   [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
   [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
@@ -141,7 +169,9 @@ print(sudoku_validator(
   [ 7, 1, 3, 9, 2, 4, 8, 5, 6 ],
   [ 9, 6, 1, 5, 3, 7, 2, 8, 4 ],
   [ 2, 8, 7, 4, 1, 9, 6, 3, 5 ],
-  [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ] ]))print(sudoku_validator([
+  [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ] ]))
+
+print(sudoku_validator([
   [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
   [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
   [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],

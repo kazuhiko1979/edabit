b8233ed5f06202ff833ec5c98d4206a6ceb36c65
kazuhiko1979 <challengevolley1979@gmail.com>
2023-08-11 14:43:54 +0900

    initial

---
+++ 100_very_hard_Evaluating Simple Algebra.py
@@ -0,0 +1,57 @@
+"""
+Evaluating Simple Algebra
+Given a string containing an algebraic equation, calculate and return the value of x. You'll only be given equations for simple addition and subtraction.
+
+Examples
+eval_algebra("2 + x = 19") ➞ 17
+
+eval_algebra("4 - x = 1") ➞ 3
+
+eval_algebra("23 + 1 = x") ➞ 24
+Notes
+There are spaces between every number and symbol in the string.
+x may be a negative number.
+"""
+
+def eval_algebra(eq):
+
+	eq = eq.split(' ')
+	# print(eq)
+
+	if eq[-1] == 'x':
+		return eval(''.join(eq[:-2]))
+
+	if eq[0] == 'x':
+		if int(eq[2]) >= int(eq[-1]):
+			if eq[1] == '+':
+				return eval(''.join(eq[-1] + '-' + eq[2]))
+			if eq[1] == '-':
+				return eval(''.join(eq[-1] + '+' + eq[2]))
+		elif int(eq[2]) <= int(eq[-1]):
+			if eq[1] == '-':
+				return eval(''.join(eq[-1] + '+' + eq[2]))
+			if eq[1] == '+':
+				return eval(''.join(eq[-1] + '-' + eq[2]))
+	if eq[2] == 'x':
+		if int(eq[0]) >= int(eq[-1]):
+			if eq[1] == '+':
+				return -(eval(''.join('-' + eq[-1] + '+' + eq[0])))
+			if eq[1] == '-':
+				return eval(''.join('-' + eq[-1] + '+' + eq[0]))
+		elif int(eq[0]) <= int(eq[-1]):
+			if eq[1] == '-':
+				return eval(''.join(eq[-1] + '+' + eq[0]))
+			if eq[1] == '+':
+				return eval(''.join(eq[-1] + '-' + eq[0]))
+
+print(eval_algebra("2 + x = 19"))
+print(eval_algebra("4 - x = 1"))
+print(eval_algebra("23 + 1 = x"))
+print(eval_algebra("25 - 1 = x"))
+print(eval_algebra("x + 10 = 53"))
+print(eval_algebra("-23 + x = -20"))
+print(eval_algebra("10 + x = 5"))
+print(eval_algebra("-49 - x = -180"))
+print(eval_algebra("x + 118 = 151"))
+print(eval_algebra("x - 46 = -2"))
+print(eval_algebra("-29 - x = -170"))

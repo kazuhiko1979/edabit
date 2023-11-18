7f913b37ffb6d63fb6498f2469a38ccc501eac6e
kazuhiko1979 <challengevolley1979@gmail.com>
2023-11-18 20:43:37 +0900

    update py js java

--- 119_very_hard_Minimum_Steps_to_a_Palindrome.py
+++ 119_very_hard_Minimum_Steps_to_a_Palindrome.py
@@ -15,21 +15,26 @@ Notes
 Trivially, words which are already palindromes should return 0.
 """
 
+
 def min_palindrome_steps(txt):
 
-	count = 0
-	temp = ""
-	if txt == txt[::-1]:
-		return count
-	for i in txt:
-		temp += i
-		temp = temp[::-1]
-		result = txt + temp
-		count += 1
-		if result == result[::-1]:
-			return count
-		else:
-			temp = temp[::-1]
+    for i in range(len(txt)):
+        if txt[i:] == txt[i:][::-1]:
+            return i
+
+        # count = 0
+        # temp = ""
+        # if txt == txt[::-1]:
+        #     return count
+        # for i in txt:
+        #     temp += i
+        #     temp = temp[::-1]
+        #     result = txt + temp
+        #     count += 1
+        #     if result == result[::-1]:
+        #         return count
+        #     else:
+        #         temp = temp[::-1]
 
 
 print(min_palindrome_steps("race"))
@@ -44,6 +49,3 @@ print(min_palindrome_steps("www"))
 print(min_palindrome_steps("me"))
 print(min_palindrome_steps("rorr"))
 print(min_palindrome_steps("pole"))
-
-
-

a0ab2c3a5cbd6b01615650720c9c98b026047c04
kazuhiko1979 <challengevolley1979@gmail.com>
2023-12-17 19:08:37 +0900

    improved java

--- 123_very_hard_Same_Letter_Patterns.js
+++ 123_very_hard_Same_Letter_Patterns.js
@@ -1,22 +1,38 @@
-function sameLetterPattern(txt1, txt2){
-  function generatePattern(txt){
-    let temp = {};
-    let pattern = '';
-    let currentPattern = 1;
-
-    for (let char of txt){
-      if (!temp[char]){
-        temp[char] = currentPattern;
-        currentPattern +=1;
-      }
-      pattern += temp[char];
-    }
-
-  return pattern;
-  }
-
-  return generatePattern(txt1) === generatePattern(txt2);
+function sameLetterPattern(txt1, txt2) {
+    return allPos(txt1).toString() === allPos(txt2).toString();
 }
+
+function allPos(x) {
+    let positions = [];
+    for (let i = 0; i < x.length; i++) {
+        for (let j = 0; j < x.length; j++) {
+            if (x[i] === x[j]) {
+                positions.push(j);
+            }
+        }
+    }
+    return positions;
+}
+
+// function sameLetterPattern(txt1, txt2){
+//   function generatePattern(txt){
+//     let temp = {};
+//     let pattern = '';
+//     let currentPattern = 1;
+
+//     for (let char of txt){
+//       if (!temp[char]){
+//         temp[char] = currentPattern;
+//         currentPattern +=1;
+//       }
+//       pattern += temp[char];
+//     }
+
+//   return pattern;
+//   }
+
+//   return generatePattern(txt1) === generatePattern(txt2);
+// }
 
 
 // Examples
